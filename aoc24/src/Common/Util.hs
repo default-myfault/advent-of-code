136bfde182dc16b378a670a48d05f436c3380f23
Henning Thomann <henning.thomann@gmail.com>
2024-12-02 22:40:07 +0100

    +day 2 2024

--- aoc24/src/Common/Util.hs
+++ aoc24/src/Common/Util.hs
@@ -5,16 +5,21 @@ type Solution = String -> Int
 -- | reverse function application
 (&) :: a -> (a -> b) -> b
 x & f = f x
+
 infixl 1 &
+
+allEq :: (Eq a) => [a] -> Bool
+allEq [] = True
+allEq (x : xs) = all (x ==) xs
 
 zipWith' :: (a -> a -> b) -> [[a]] -> [b]
 zipWith' f [xs, ys] = zipWith f xs ys
 zipWith' _ _ = error "list has to have a length of 2"
 
-countElem :: Eq a => a -> [a] -> Int
+countElem :: (Eq a) => a -> [a] -> Int
 countElem x ls = go ls 0
-  where
-    go [] acc = acc
-    go (y:ys) acc
-      | y == x = go ys (acc + 1)
-      | otherwise = go ys acc
+ where
+  go [] acc = acc
+  go (y : ys) acc
+    | y == x = go ys (acc + 1)
+    | otherwise = go ys acc
--- aoc24/src/Day02.hs
+++ aoc24/src/Day02.hs
@@ -1,12 +1,51 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
 module Day02 (solve) where
 
-import Common.Util (Solution)
+import Common.Util (Solution, allEq, countElem)
+import Data.List (permutations)
 
 solve :: (Solution, Solution)
 solve = (solve1, solve2)
 
+example :: String
+example =
+  "\
+  \7 6 4 2 1\n\
+  \1 2 7 8 9\n\
+  \9 7 6 2 1\n\
+  \1 3 2 4 5\n\
+  \8 6 4 4 1\n\
+  \1 3 6 7 9\n\
+  \"
+
+parse :: String -> [[Int]]
+parse = map (map read . words) . lines
+
+safe :: [Int] -> Bool
+safe xs = monotone && inRange
+ where
+  deltas = zipWith (-) xs (tail xs)
+
+  monotone = all (> 0) deltas || all (< 0) deltas
+  inRange = all ((<= 3) . abs) deltas
+
+-- >>> map (\xs -> (filter (\ls -> length ls == length xs) $ permutations xs)) (parse example)
+
+-- >>> solve1 example
+-- 2
+
 solve1 :: Solution
-solve1 = undefined
+solve1 = length . filter safe . parse
+
+-- >>> solve2 example
+-- 4
 
 solve2 :: Solution
-solve2 = undefined
+solve2 = length . filter (any safe) . map missingOne . parse
+ where
+  missingOne xs = [remove i xs | i <- [0 .. length xs - 1]]
+
+  remove 0 (_ : xs) = xs
+  remove n (x : xs) = x : remove (n - 1) xs
